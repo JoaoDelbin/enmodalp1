3440d5fc684c40c5db10e0feba69d4f3e6e42242
JoaoDelbin <joao.delbin17@gmail.com>
2024-12-10 00:19:43 -0300

    finalizando, faltando videos depoimentos

--- js/script.js
+++ js/script.js
@@ -52,3 +52,35 @@ function playVideo() {
     document.querySelector('.video-thumbnail').style.display = 'none'; 
   }
 
+
+//   contaagem 2 final do site 
+
+// Define a data e hora de término da nova contagem regressiva
+const novaCountdownDate = new Date("Dec 5, 2024 23:59:59").getTime();
+
+// Atualiza a contagem regressiva a cada segundo
+const y = setInterval(function() {
+    // Obtém a data e hora atuais
+    const now = new Date().getTime();
+
+    // Calcula a diferença entre a data de término e a data atual
+    const distance = novaCountdownDate - now;
+
+    // Cálculos para dias, horas, minutos e segundos
+    const novaDays = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const novaHours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const novaMinutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const novaSeconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Exibe os resultados na página
+    document.getElementById("nova-days").innerHTML = String(novaDays).padStart(2, '0');
+    document.getElementById("nova-hours").innerHTML = String(novaHours).padStart(2, '0');
+    document.getElementById("nova-minutes").innerHTML = String(novaMinutes).padStart(2, '0');
+    document.getElementById("nova-seconds").innerHTML = String(novaSeconds).padStart(2, '0');
+
+}, 1000);
+
+// faq
+
+// Seleciona todos os botões de pergunta
+
